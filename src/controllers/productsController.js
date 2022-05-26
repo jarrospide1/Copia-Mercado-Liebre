cf3da95a59701258e90a15990b96c14a158a8f90
jarrospide1 <juanarrospidepons@gmail.com>
2022-05-26 17:23:47 -0400

    Importacion con avances de rutas, Controladores y vistas

---
+++ src/controllers/productsController.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+let productsArray = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products', { productsArray });
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		const theProduct = productsArray.find( oneProduct => oneProduct.id === Number(req.params.id));
+		res.render('detail', { theProduct: theProduct });
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render('product-create-form');
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		//generar ID
+		const generateId = () => {
+			// 1. Obtenemos el ultimo producto almacenado en la DB
+			const lastProduct = productsArray[productsArray.length - 1];
+			// 2. Obtenemos el ID de ese ultimo producto
+			const lastId = lastProduct.id;
+			// 3. Retornamos ese ultimo Id incrementado en 1
+			return lastId + 1
+		}
+/*
+		productsArray.push({
+			id: generateId(),
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description,
+			image: req.file.filename
+		});
+*/		
+		productsArray.push({
+			id: generateId(),
+			...req.body,
+			image: req.file.filename
+			
+		});
+		
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(productsArray, null, ' '));
+
+		res.redirect('/products');
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		const productToEdit = productsArray.find(oneProduct => oneProduct.id === Number(req.params.id));
+		res.render('product-edit-form', {productToEdit: productToEdit});
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		let productToEdit = productsArray.find(oneProduct => oneProduct.id === Number(req.params.id));
+
+		productToEdit = {
+			...req.body,
+			//image: req.file.filename
+		}
+
+		res.redirect('/product')
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		productsArray = productsArray.filter( oneProduct => oneProduct.id !== Number(req.params.id));
+		fs.writeFileSync(productsFilePath, JSON.stringify(productsArray, null, ' '));
+
+		res.redirect('/products');
+
+	}
+};
+
+module.exports = controller;
---
+++ src/routes/products.js
@@ -0,0 +1,44 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+
+// ************ Multer ************
+
+const multerDiskStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve(__dirname, '../../public/images/products'))
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1E9)
+        cb(null, uniqueSuffix + '_' + file.fieldname + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({ storage: multerDiskStorage });
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create/', productsController.create); 
+router.post('/', upload.single('image'), productsController.store); 
+
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/:id/', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/:id/edit', productsController.edit); 
+router.put('/:id', productsController.update); 
+
+
+/*** DELETE ONE PRODUCT ***/ 
+router.delete('/:id', productsController.destroy); 
+
+
+module.exports = router;
